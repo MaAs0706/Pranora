e6dc8c77f04d22b71c59fb65f5a7bb6c59af3dcb
MaAs0706e <aswanthmadhav07@gmail.com>
2025-06-10 13:43:40 +0530

    changes

--- frontend /script.js
+++ frontend /script.js
@@ -1,7 +1,17 @@
 
 
-document.getElementById('reportForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert('Report submitted! (Functionality coming soon)');
+ocument.getElementById('reportForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const photo = document.getElementById('photo').files[0];
+  const location = document.getElementById('location').value;
+  const description = document.getElementById('description').value;
+
+  console.log({
+    photo,
+    location,
+    description
   });
-  
+
+  alert('Report submitted! (Logged to console for now)');
+});
